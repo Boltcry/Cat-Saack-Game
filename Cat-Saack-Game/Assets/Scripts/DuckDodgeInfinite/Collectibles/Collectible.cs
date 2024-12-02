c263409268c4e5440643837c1e43a7be7f64556c
Boltcry <crystal.hicks@utexas.edu>
2024-12-02 10:55:18 -0600

    Add Animation V3
    Added pickup particles for powerups
    Fixed bug where collectibles would stop spawning after a pause
    
    Exported build 4.0

--- Cat-Saack-Game/Assets/Scripts/DuckDodgeInfinite/Collectibles/Collectible.cs
+++ Cat-Saack-Game/Assets/Scripts/DuckDodgeInfinite/Collectibles/Collectible.cs
@@ -9,6 +9,9 @@ public class Collectible : MonoBehaviour
     public bool incrementTokenCount = false;
     [Tooltip("Name of the animation played on collect")]
     public string collectAnimationName;
+    [Tooltip("Particle system that plays on OnCollect")]
+    [SerializeField]
+    ParticleSystem collectParticle;
 
     Collider2D triggerCollider;
     CollectibleSpawner spawner;
@@ -20,7 +23,7 @@ public class Collectible : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    protected virtual void OnCollect()
+    protected virtual IEnumerator OnCollect()
     {
         //Debug.Log("Collectible collected: "+this);
 
@@ -29,21 +32,40 @@ public class Collectible : MonoBehaviour
         {
             AudioManager.PlayAudioClip(AudioType.SFX, collectSound);
         }
+
         // play collection animation
+        if (collectParticle != null)
+        {
+            // disable sprite
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            if (sprite != null)
+            {
+                sprite.enabled = false;
+            }
+
+            collectParticle.Play();
+            yield return StartCoroutine(WaitForParticles());
+        }
+
         // add points, or apply powerup, or whateverI 
         if (incrementTokenCount)
         {
             MinigameManagerDuck.IncrementTokensCollected();
         }
 
-        StartCoroutine(DestroyAfterAnimation());
+        if (!collectAnimationName.Equals(""))
+        {
+            yield return StartCoroutine(WaitForCloseAnimation());
+        }
+
+        DestroyCollectible();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            OnCollect();
+            StartCoroutine(OnCollect());
         }
     }
 
@@ -57,7 +79,7 @@ public class Collectible : MonoBehaviour
         }
     }
 
-    private IEnumerator DestroyAfterAnimation()
+    private IEnumerator WaitForCloseAnimation()
     {
         if (animator != null)
         {
@@ -67,8 +89,14 @@ public class Collectible : MonoBehaviour
             yield return new WaitUntil(() => 
                 animator.GetCurrentAnimatorStateInfo(0).IsName(collectAnimationName));
         }
+    }
 
-        DestroyCollectible();
+    private IEnumerator WaitForParticles()
+    {
+        while (collectParticle.isPlaying)
+        {
+            yield return null;
+        }
     }
 
     public void RegisterSpawner(CollectibleSpawner aSpawner)
