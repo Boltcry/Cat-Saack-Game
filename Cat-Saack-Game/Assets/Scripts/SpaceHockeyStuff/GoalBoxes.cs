45586adbe6d76d91aca5f11ce71e2b106a9b7722
SleepyFlowers <kmcele@gmail.com>
2024-12-08 19:32:44 -0600

    Hockey Table connected to Lobby

--- Cat-Saack-Game/Assets/Scripts/SpaceHockeyStuff/GoalBoxes.cs
+++ Cat-Saack-Game/Assets/Scripts/SpaceHockeyStuff/GoalBoxes.cs
@@ -30,7 +30,7 @@ public class GoalBoxes : MonoBehaviour
     void ResetPuck()
     {
         GameObject puck = GameObject.FindGameObjectWithTag("Puck");
-        puck.transform.position = new Vector2(0f, 0f);
+        puck.transform.position = new Vector2(-.29f, 0f);
         Rigidbody2D rb = puck.GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.zero;
 
